6d94521b4495ca2148b60a6778bffdfe842e099d
rogervezaro <rogervezaro1997@gmail.com>
2020-09-23 13:56:11 -0400

    feat: Add style for edit modal

--- lib/plantar_web/live/modal_component.ex
+++ lib/plantar_web/live/modal_component.ex
@@ -4,15 +4,17 @@ defmodule PlantarWeb.ModalComponent do
   @impl true
   def render(assigns) do
     ~L"""
-    <div id="<%= @id %>" class="phx-modal"
+    <div id="<%= @id %>" class="fixed overflow-y-scroll inset-0 flex items-center justify-center"
+      style="background-color: rgba(0,0,0,0.3);"
       phx-capture-click="close"
       phx-window-keydown="close"
       phx-key="escape"
       phx-target="#<%= @id %>"
       phx-page-loading>
 
-      <div class="phx-modal-content">
+      <div class="max-w-lg p-8 sm:pb-4 bg-white rounded shadow-lg text-center sm:text-left">
         <%= live_patch raw("&times;"), to: @return_to, class: "phx-modal-close" %>
+
         <%= live_component @socket, @component, @opts %>
       </div>
     </div>
